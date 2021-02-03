77d42008a4fb2c13829b25ca0097faf192be0d51
Ray <shanei5107@gmail.com>
2021-02-03 10:24:02 +0800

    update: project init

---
+++ src/main/java/com/ray/coding/RayCodingApplication.java
@@ -0,0 +1,21 @@
+package com.ray.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ray-coding 代码生成
+ *
+ * @author ray
+ * @since 2020-2-3
+ */
+@SpringBootApplication
+public class RayCodingApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RayCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ray/coding/RayCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
